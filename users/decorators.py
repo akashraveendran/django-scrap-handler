00e76324b8f2495406986cd5c671f8ddea87b3d7
Akash Raveendran <akashraveendran1611@gmail.com>
2022-11-18 07:57:06 +0530

    added user only decorator

---
+++ users/decorators.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect
+
+
+def user_only(view_func):
+
+    def wrapper_function(request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return view_func(request, *args, **kwargs)
+        else:
+            return redirect('signin')
+
+    return wrapper_function
