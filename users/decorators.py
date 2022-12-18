0013fc595ab6c3a4754a81a0c60a73ff1620c179
Akash Raveendran <akashraveendran1611@gmail.com>
2022-12-18 12:19:50 +0530

    updated decorator for users

--- users/decorators.py
+++ users/decorators.py
@@ -4,7 +4,10 @@ from django.shortcuts import redirect
 def user_only(view_func):
 
     def wrapper_function(request, *args, **kwargs):
-        if request.user.is_authenticated:
+        group = None
+        if request.user.groups.exists():
+            group = request.user.groups.all()[0].name
+        if group == 'users':
             return view_func(request, *args, **kwargs)
         else:
             return redirect('home')
@@ -15,7 +18,10 @@ def user_only(view_func):
 def not_auth_user(view_func):
 
     def wrapper_function(request, *args, **kwargs):
-        if request.user.is_authenticated:
+        group = None
+        if request.user.groups.exists():
+            group = request.user.groups.all()[0].name
+        if group == 'users':
             return redirect('index')
         else:
             return view_func(request, *args, **kwargs)
