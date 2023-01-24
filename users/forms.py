9d48722c8e14379de6d1332d178ca564a878a743
Akash Raveendran <akashraveendran1611@gmail.com>
2023-01-24 15:39:39 +0530

    changed signup signin and home page of users

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,4 @@
-from django.forms import forms
+from django.forms import forms,TextInput,PasswordInput
 from django.contrib.auth.forms import UserCreationForm
 
 from django.contrib.auth.models import User
@@ -10,3 +10,13 @@ class UserAddForm(UserCreationForm):
         model = User
         fields = ["username", "first_name", "last_name",
                   "email", "password1", "password2"]
+        widgets = {
+            'username': TextInput(attrs={'placeholder':'Enter Username'}),
+            'first_name': TextInput(attrs={'placeholder':'Enter Firstname'}),
+            'last_name': TextInput(attrs={'placeholder':'Enter Lastname'}),
+            'email': TextInput(attrs={'placeholder':'Enter Email'}),
+        }
+    def __init__(self, *args, **kwargs) :
+        super().__init__(*args, **kwargs)
+        self.fields["password1"].widget.attrs.update({'placeholder':"Enter Password"})
+        self.fields["password2"].widget.attrs.update({'placeholder':"Confirm Password"})
